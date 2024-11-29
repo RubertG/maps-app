01e25ae687aab8b80ea61660e93d66e73286a25b
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-28 22:59:18 -0500

    nuevo: validar geolocalizacion

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if ( !navigator.geolocation ) {
+  alert('Tu navegador no soporta la geolocalización :(')
+  throw new Error('Geolocation is not supported by your browser')
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
