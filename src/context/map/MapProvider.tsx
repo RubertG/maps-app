2028fd91cd91366ddee482f99788e4cc0089b958
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-01 15:03:32 -0500

    nuevo: nuevas funciones del provider de mapa

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,22 +1,57 @@
-import { useReducer } from "react"
+import { useContext, useEffect, useReducer } from "react"
 import { Map, Marker, Popup } from "mapbox-gl"
 import { mapReducer } from "./mapReducer"
 import { MapContext } from "./MapContext"
+import { PlacesContext } from "../places/PlacesContext"
 
 export interface MapState {
   isMapReady: boolean
   map?: Map
+
+  markers: Marker[]
 }
 
 const INITIAL_STATE: MapState = {
   isMapReady: false,
-  map: undefined
+  map: undefined,
+  markers: []
 }
 
 const MapProvider = ({
   children
 }: React.PropsWithChildren) => {
   const [state, dispatch] = useReducer(mapReducer, INITIAL_STATE)
+  const { places } = useContext(PlacesContext)
+
+  useEffect(() => {
+    if (places.length === 0) {
+      state.markers.forEach(marker => marker.remove())
+      return 
+    }
+
+    state.markers.forEach(marker => marker.remove())
+    const newMarkers: Marker[] = []
+
+    for (const place of places) {
+      const [lng, lat] = place.geometry.coordinates
+
+      const popup = new Popup().setHTML(`
+        <h6 class="text-lg font-bold">${place.properties.name}</h6>
+        <p class="text-xs text-zinc-700">${place.properties.full_address}</p>
+      `)
+
+      const newMarker = new Marker()
+        .setLngLat([lng, lat])
+        .setPopup(popup)
+        .addTo(state.map!)
+
+      newMarkers.push(newMarker)
+    }
+
+    dispatch({ action: 'setMarkers', payload: newMarkers })
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [places])
 
   const setMap = (map: Map) => {
     const myLocationPopup = new Popup()
--- src/context/map/mapReducer.ts
+++ src/context/map/mapReducer.ts
@@ -1,13 +1,19 @@
-import { Map } from "mapbox-gl"
+import { Map, Marker } from "mapbox-gl"
 import { MapState } from "./MapProvider"
 
 type MapAction = { action: 'setMap', payload: Map }
+                | { action: 'setMarkers', payload: Marker[] }
 
 export const mapReducer = (state: MapState, action: MapAction): MapState => {
   if (action.action === 'setMap') return {
     ...state,
     isMapReady: true,
     map: action.payload
+  }
+
+  if (action.action === 'setMarkers') return {
+    ...state,
+    markers: action.payload
   }
 
   return state
