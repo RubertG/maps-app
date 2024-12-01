a634a0538161623a8e45c925a41df88fcb3f5004
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-01 10:26:40 -0500

    nuevo: Cargar token de acceso

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = import.meta.env.VITE_ACCESS_TOKEN
 
 if ( !navigator.geolocation ) {
   alert('Tu navegador no soporta la geolocalización :(')
