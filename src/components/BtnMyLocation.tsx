b18fcc5eaf6e7c613b6b0f0cfe49fe8a240ebc4a
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-01 15:02:48 -0500

    nuevo: desactivar lugar activo

--- src/components/BtnMyLocation.tsx
+++ src/components/BtnMyLocation.tsx
@@ -3,13 +3,14 @@ import { MapContext, PlacesContext } from "../context"
 
 const BtnMyLocation = () => {
   const { isMapReady, map } = useContext(MapContext)
-  const { userLocation } = useContext(PlacesContext)
+  const { userLocation, setActivePlaceId } = useContext(PlacesContext)
 
   const onClick = () => {
     if (!isMapReady) throw new Error('Mapa no está listo')
     if (!userLocation) throw new Error('No hay ubicación del usuario')
 
     map?.flyTo({ center: userLocation, zoom: 16 })
+    setActivePlaceId('')
   }
 
   return (
